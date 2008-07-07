a195154c6597765d135830e1bb608239225a2a48
Martin Aspeli <optilude@gmail.com>
2008-07-07 00:08:50 +0000

    Simplify the behavior example to use an annotation factory directly.
    svn path=/example.dexterity/trunk/; revision=67958

--- example/dexterity/behavior.py
+++ example/dexterity/behavior.py
@@ -10,67 +10,29 @@ from zope.annotation import factory
 
 from BTrees.OOBTree import OOSet
 
-class ITaggingBehavior(Interface):
+class ITagging(Interface):
     """Behavior interface to make a type support tagging.
-    If this is listed as enabled in the FTI, then we will be able to
-    adapt a content item of the given type to this interface.
     """
     
-    enabled = schema.Bool(title=u"Enable tagging")
+    enabled = schema.Bool(title=u"Tagging enabled",
+                          required=False,
+                          default=True)
     
-    def add(tag):
-        """Add a tag
-        """
-    
-    def remove(tag):
-        """Remove a tag
-        """
-    
-    def tags():
-        """List tags
-        """
-# Implementation of the behavior
-    
-class ITaggingAnnotations(Interface):
-    """Specifies annotations that store tagging data
-    """
-    
-    enabled = schema.Bool(title=u"Enabled")
-    tags = schema.Set(title=u"Tags", value_type=schema.TextLine(title=u"Tag"))
+    tags = schema.List(title=u"Tags",
+                       description=u"Tags for this object",
+                       value_type=schema.Choice(values=["Tag 1", "Tag 2", "Tag 3"]),
+                       required=False)
 
 class TaggingAnnotations(Persistent):
-    """Persistent storage for tags
+    """Persistent storage for tags in annotations
     """
     
-    implements(ITaggingAnnotations)
+    implements(ITagging)
     adapts(IAnnotatable)
     
     def __init__(self):
-        self.enabled = False
+        self.enabled = True
         self.tags = OOSet()
 
-TaggingAnnotationsFactory = factory(TaggingAnnotations)
-
-class TaggingBehavior(object):
-    """Implementation of the behavior that stores data in annotations.
-    """
-
-    implements(ITaggingBehavior)
-    
-    def __init__(self, context):
-        self.data = ITaggingAnnotations(context)
-        
-    def _get_enabled(self):
-        return self.data.enabled
-    def _set_enabled(self, value):
-        self.data.enabled = value
-    enabled = property(_get_enabled, _set_enabled)
-    
-    def add(self, tag):
-        self.data.tags.add(tag)
-        
-    def remove(self, tag):
-        self.data.tags.remove(tag)
-        
-    def tags(self):
-        return list(self.data.tags)
+# Use the factory from zope.annotation to support persistent storage of tag data.
+Tagging = factory(TaggingAnnotations)
