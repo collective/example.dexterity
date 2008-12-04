ecdc66eb2c88f24a1ab5b4d673f152ec6d1ef2c6
David Glick <davidglick@groundwire.org>
2008-12-04 05:27:48 +0000

    added test suite for benchmarking dexterity content vs. archetypes content
    svn path=/example.dexterity/trunk/; revision=76804

--- example/dexterity/tests.py
+++ example/dexterity/tests.py
@@ -1,19 +1,13 @@
 import unittest
 import example.dexterity
 
-from zope.testing import doctestunit
-from zope.component import testing, getMultiAdapter
-
-from zope.component import createObject
+from zope.component import getMultiAdapter
 
 from zope.lifecycleevent import modified
 
-from Testing import ZopeTestCase as ztc
-
 from Products.Five import zcml
-from Products.Five import fiveconfigure
 from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import PloneSite, onsetup
+from Products.PloneTestCase.layer import onsetup
 
 from example.dexterity.page import IPage
 from example.dexterity.pypage import PyPage
@@ -95,5 +89,76 @@ class IntegrationTests(ptc.PloneTestCase):
         fspage = addform.createAndAdd({})
         editview = getMultiAdapter((fspage, request), name=u"edit")
 
+from Products.Five.testbrowser import Browser
+from time import time
+import gc
+
+BENCHMARK_REPS = 50
+def benchmark(func):
+    def benchmarked_func(self):
+        # prime the pumps
+        func(self)
+        # temporarily disable garbage collection
+        gc.disable()
+        t0 = time()
+        for i in xrange(0, BENCHMARK_REPS):
+            func(self)
+        t1 = time()
+        gc.enable()
+        elapsed = t1 - t0
+        print "\n%s: %s\n" % (func.__name__, elapsed)
+    return benchmarked_func
+
+class Benchmarks(ptc.FunctionalTestCase):
+
+    def afterSetUp(self):
+        self.browser = Browser()
+        self.browser.handleErrors = False
+        self.portal_url = 'http://nohost/plone/'
+        
+        # log in
+        self.app.acl_users.userFolderAddUser('root', 'secret', ['Manager'], [])
+        self.browser.addHeader('Authorization', 'Basic root:secret')
+        self.setRoles(['Manager'])
+        
+        # add a sample page (AT and dexterity)
+        self.app.plone.invokeFactory('Document', 'atdoc')
+        self.app.plone.invokeFactory('example.ttwpage', 'dexdoc')
+
+    @benchmark
+    def test_view_AT_page(self):
+        self.browser.open(self.portal_url + 'atdoc')
+    
+    @benchmark
+    def test_view_dexterity_page(self):
+        self.browser.open(self.portal_url + 'dexdoc')
+    
+    @benchmark
+    def test_AT_page_addform(self):
+        self.browser.open(self.portal_url + 'createObject?type_name=Document')
+    
+    @benchmark
+    def test_dexterity_page_addform(self):
+        self.browser.open(self.portal_url + '@@add-dexterity-content/example.ttwpage')
+
+    @benchmark
+    def test_create_AT_page(self):
+        self.browser.open(self.portal_url + 'createObject?type_name=Document')
+        self.browser.getControl('Title').value = 'atdoc2'
+        self.browser.getControl('Save').click()
+        self.failUnless('atdoc2' in self.browser.url)
+
+    @benchmark
+    def test_create_dexterity_page(self):
+        self.browser.open(self.portal_url + '@@add-dexterity-content/example.ttwpage')
+        self.browser.getControl('Title').value = 'dexdoc2'
+        self.browser.getControl('Save').click()
+        # XXX change this once we have title-to-id functionality
+        self.failUnless('example.ttwpage' in self.browser.url)
+
+
 def test_suite():
-    return unittest.TestSuite([unittest.makeSuite(IntegrationTests)])
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(IntegrationTests))
+    suite.addTest(unittest.makeSuite(Benchmarks))
+    return suite
